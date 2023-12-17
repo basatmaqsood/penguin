7eb64df997dc2b51e1c796bc7310973d6334f615
basat <itzbasatmaqsood@gmail.com>
2023-12-17 21:37:21 +0500

    first version complete

--- script/script.js
+++ script/script.js
@@ -2,8 +2,10 @@
 const toggle = document.getElementById('toggle-btn');
 const navList = document.getElementById('nav-list');
 
+
 toggle.addEventListener('click', () => {
   navList.classList.toggle('nav-list-drawer');
   navList.classList.toggle('nav-list-hidden');
-
 });
+
+
